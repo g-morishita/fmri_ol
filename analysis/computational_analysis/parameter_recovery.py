5eeba11088b2e47b811fe30c4e3181272aeff283
Gota Morishita <optimistic.gota@gmail.com>
2025-07-10 12:19:14 +0900

    implement parameter recovery

---
+++ analysis/computational_analysis/parameter_recovery.py
@@ -0,0 +1,44 @@
+import numpy as np
+from analysis.computational_analysis.nll import nll_per_session_full
+from analysis.computational_analysis.simulator import  FullModel, AsocialRewardModel
+import analysis.computational_analysis.estimator as estimator
+
+
+class ParameterRecovery:
+    def __init__(self, self_model, partner_model, reward_probs):
+        self.self_model = self_model
+        self.partner_model = partner_model
+        self.reward_probs = reward_probs
+        self.n_blocks = 2
+        self.n_trials = 30
+        self.set_partner_rewards = []
+        self.set_partner_choices = []
+        self.set_self_choices = []
+     
+    def simulate_block(self):
+        self.self_model.reset()
+        self.partner_model.reset()
+        
+        partner_rewasds = []
+        partner_choices = []
+        self_choices = []
+        for _ in range(self.n_trials):
+            partner_choice = self.partner_model.make_choice()
+            partner_reward = int(np.random.uniform() < self.reward_probs[partner_choice])
+            self.partner_model.update_values(partner_reward, partner_choice)
+
+            partner_choices.append(partner_choice)
+            partner_rewasds.append(partner_reward)
+
+            self.self_model.update_values(partner_reward, partner_choice)
+            self_choice = self.self_model.make_choice()
+            self_choices.append(self_choice)
+        
+        self.set_partner_rewards.append(partner_rewasds)
+        self.set_partner_choices.append(partner_choices)
+        self.set_self_choices.append(self_choices)
+
+    def fit(self, estimator):
+        # Fit the model to the simulated data
+        params, nll = estimator.fit(self.set_partner_rewards, self.set_partner_choices, self.set_self_choices)
+        return params
