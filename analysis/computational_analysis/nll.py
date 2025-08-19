9cfc51a7f73a35182e01f61b0786a36ec2644597
Gota Morishita <optimistic.gota@gmail.com>
2025-08-19 21:14:18 +1000

    update negative log-likelihood function

--- analysis/computational_analysis/nll.py
+++ analysis/computational_analysis/nll.py
@@ -2,6 +2,10 @@ import numpy as np
 from scipy.special import softmax
 
 
+def _skip_choice(sc):
+    return sc is None or (isinstance(sc, float) and np.isnan(sc))
+
+
 def update_V(V, r, c, lr):
     delta = r - V[c]
     V[c] += lr * delta
@@ -18,6 +22,7 @@ def update_A(A, c, lr):
             delta = 0 - A[a]
             A[a] += lr * delta
 
+
 def get_choice_prob(Q, beta):
     p = softmax(beta * Q)
     return p
@@ -28,7 +33,7 @@ def nll(params, nll_per_session, set_other_rewards, set_other_choices, set_self_
 
     nll = 0
     for i in range(n_sessions):
-        nll += nll_per_session(params, set_other_choices[i], set_other_choices[i], set_self_choices[i])
+        nll += nll_per_session(params, set_other_rewards[i], set_other_choices[i], set_self_choices[i])
 
     return nll
 
@@ -45,11 +50,11 @@ def nll_per_session_full(params, other_rewards, other_choices, self_choices):
         update_A(A, oc, action_lr)
 
         # Ignore cases where self choice is None (mishit)
-        if np.isnan(sc):
+        if _skip_choice(sc):
             continue
 
         # Calculate choice probability
-        Q = (1 - weight_for_A) * V + weight_for_A * V
+        Q = (1 - weight_for_A) * V + weight_for_A * A
         choice_prob = get_choice_prob(Q, beta)
         nll -= np.log(choice_prob[int(sc)] + 1e-10)  # Adding a small value to avoid log(0)
 
@@ -66,7 +71,7 @@ def nll_per_session_q(params, other_rewards, other_choices, self_choices):
         update_V(V, r, oc, reward_lr)
 
         # Ignore cases where self choice is None (mishit)
-        if sc is None:
+        if _skip_choice(sc):
             continue
         # Calculate choice probability
         choice_prob = get_choice_prob(V, beta)
@@ -85,7 +90,7 @@ def nll_per_session_action(params, other_rewards, other_choices, self_choices):
         update_A(A, oc, action_lr)
 
         # Ignore cases where self choice is None (mishit)
-        if sc is None:
+        if _skip_choice(sc):
             continue
         # Calculate choice probability
         choice_prob = get_choice_prob(A, beta)
