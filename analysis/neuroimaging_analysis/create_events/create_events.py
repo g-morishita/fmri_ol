2f20c829bc83fab33a59dac4b3b7af0f31f69896
Gota Morishita <optimistic.gota@gmail.com>
2025-09-09 21:32:49 +1000

    update create events

--- analysis/neuroimaging_analysis/create_events/create_events.py
+++ analysis/neuroimaging_analysis/create_events/create_events.py
@@ -8,10 +8,10 @@ class Setting:
     MODEL_PARAM_PATH = BASE_DIR / "../../computational_analysis/fit_const_weight_full_real_data/results/fit_summary.csv"
     BEHAVIORAL_DATA_DIR = BASE_DIR / "../../../data/behavioral_data/"
     EVENT_DATA_PATH = BASE_DIR / "../../../data/events/"
-    SUBJECTS = list(range(1, 19)) + list(range(20, 31))  # Exclude subject 19 due to missing data
+    SUBJECTS = list(range(1, 19)) + list(range(20, 38))  # Exclude subject 19 due to missing data
 
 
-def extract_learning_rate(subject_id, partner_noise):
+def extract_learning_rates(subject_id, partner_noise):
     """
     subject_id: int, the ID of the subject (1-30)
     partner_noise: int, 1 for low noise partner, 2 for high noise partner
@@ -22,8 +22,9 @@ def extract_learning_rate(subject_id, partner_noise):
     # Extract the learning rate for the given subject
     if subject_id >= 19:
         subject_id -= 1
-    lr = df.loc[f"lr_reward[{subject_id},{partner_noise}]"]["Mean"]
-    return lr
+    lr_reward = df.loc[f"lr_reward[{subject_id},{partner_noise}]"]["Mean"]
+    lr_action = df.loc[f"lr_action[{subject_id},{partner_noise}]"]["Mean"]
+    return lr_reward, lr_action
 
 
 def extract_condition(subject_id, run):
@@ -67,9 +68,39 @@ def create_events_rpe(choice_data, lr):
     return events
 
 
-def create_events(choice_data, name):
+def create_events_ape(choice_data, lr):
+    action_tendencies = np.ones(3) / 3
+    partner_choice = choice_data["partner_choice"].tolist()
+    onsets = choice_data["t_other_choice"].tolist()
+
+    pes = []
+    for c in partner_choice:
+        pe = 1 - action_tendencies[c]
+        action_tendencies[c] += lr * pe
+        for i in range(3):
+            if i != c:
+                action_tendencies[i] += lr * (0 - action_tendencies[i])
+        pes.append(pe)
+    
+    # Z-standarize the prediction errors
+    pes = np.array(pes)
+    pes = (pes - np.mean(pes)) / np.std(pes)
+
+    events = pd.DataFrame({"onset": onsets, "duration": 0.0, "trial_type": "ape", "modulation": pes})
+    return events
+
+
+def create_events(choice_data, name, duration):
     onsens = choice_data[f"t_{name}"].tolist()
-    events = pd.DataFrame({"onset": onsens, "duration": 0.0, "trial_type": name})
+    events = pd.DataFrame({"onset": onsens, "duration": duration, "trial_type": name, "modulation": 1})
+    return events
+
+
+def create_self_choice_events(choice_data):
+    onsets_self_choice = choice_data["t_self_choice_on"]
+    onsets_self_displyed_options = choice_data["t_self_options_on"]
+    rts = onsets_self_displyed_options - onsets_self_choice
+    events = pd.DataFrame({"onset": onsets_self_choice, "duration": rts.tolist(), "trial_type": "self_choice", "modulation": 1})
     return events
 
 
@@ -79,12 +110,14 @@ def main():
             continue
         for run in range(1, 5):
             condition = extract_condition(subject_id, run)
-            lr = extract_learning_rate(subject_id, condition)
+            lr_reward, lr_action = extract_learning_rates(subject_id, condition)
             choice_data = extract_choice_data(subject_id, run)
             
-            set_events = [create_events_rpe(choice_data, lr)]
-            for name in ["self_choice_on", "other_choice"]:
-                set_events.append(create_events(choice_data, name))
+            set_events = [create_events_rpe(choice_data, lr_reward)]
+            set_events.append(create_events_ape(choice_data, lr_action))
+            set_events.append(create_self_choice_events(choice_data))
+            for name, duration in [("other_choice", 2.0), ("other_outcome", 2.0)]:
+                set_events.append(create_events(choice_data, name, duration))
 
             events = pd.concat(set_events, ignore_index=True)
             events.sort_values(by="onset", inplace=True)
